794662d06492646d3187a44f07510f86818ce592
ballakkur <37627065+ballakkur@users.noreply.github.com>
2019-01-16 18:42:57 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
+
   {path:'view',component:ViewComponent},
-  { path: ' ', redirectTo:'view',pathMatch: 'full'},
-  { path: '*', component:ViewComponent }
+
+  { path: '', redirectTo: 'view', pathMatch: 'full' },
+
+  { path: '*', component:ViewComponent },
+  { path: '**', component:ViewComponent }
+
 ];
 
 @NgModule({
